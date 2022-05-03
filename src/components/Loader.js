98e3fc55fc32629d089ab61496d03f1adf8578f5
Sanyaur <gs.ujvari@gmail.com>
2022-05-03 14:30:33 +0200

    first commit

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Orbit } from "@uiball/loaders";
+
+const Loader = () => {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Orbit size={35} color='#231F20' />
+    </div>
+  );
+};
+
+export default Loader;
